ee3732e0b7a30a351616d005487ddc1edc90f808
Kaamipacifico <psikamilapacifico@gmail.com>
2026-06-01 19:01:18 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CadastroForm from "./components/CadastroForm";
+
+function App() {
+  return (
+    <div className="container">
+      <CadastroForm />
+    </div>
+  );
+}
+
+export default App;
